7cf9fccae7ed93532f641dae942f94660a792d9c
Is_52Hertz <144510573+is52hertz@users.noreply.github.com>
2024-03-16 18:27:13 +0800

    Repos : Change Repos.
    ——
    Repos : 更改存储库。

--- cloneRepos.sh
+++ cloneRepos.sh
@@ -2,10 +2,11 @@
 set -e
 cd "$(dirname "${0}")/.."
 
-CODE_REPO="https://github.com/Clinale/OneLife.git"
-DATA_REPO="https://github.com/Clinale/OneLifeData7.git"
-GEMS_REPO="https://github.com/Clinale/minorGems.git"
+SPEEDEDUP_LINK=""
+CODE_REPO="https://github.com/X-Lives/X-Lives.git"
+DATA_REPO="https://github.com/X-Lives/XLivesData.git"
+GEMS_REPO="https://github.com/X-Lives/X-minorGems.git"
 
-if [[ ! -d OneLife ]]; then git clone $CODE_REPO OneLife; fi
-if [[ ! -d OneLifeData7 ]]; then git clone $DATA_REPO OneLifeData7; fi
-if [[ ! -d minorGems ]]; then git clone $GEMS_REPO minorGems; fi
+if [[ ! -d OneLife ]]; then git clone ${SPEEDEDUP_LINK}${CODE_REPO} OneLife; fi
+if [[ ! -d OneLifeData7 ]]; then git clone ${SPEEDEDUP_LINK}${DATA_REPO} OneLifeData7; fi
+if [[ ! -d minorGems ]]; then git clone ${SPEEDEDUP_LINK}${GEMS_REPO} minorGems; fi

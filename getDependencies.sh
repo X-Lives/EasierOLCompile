27577b6cc1f154a2e1fd1629e63962b9ada1625e
is52hertz <is.52hertz@gmail.com>
2024-03-30 15:31:12 +0800

    `Never-Ending Stream` Weekly Update : Color&Install —— `源源不断` 周末更新 : 颜色&安装

--- getDependencies.sh
+++ getDependencies.sh
@@ -2,12 +2,17 @@
 set -ex
 cd "$(dirname "${0}")/.."
 
-sudo apt-get update
+if [[ ${ELOC_install} != 1 ]];then
+	echo -e "\033[43;37m --- apt-get update --- \033[0m"
+	sudo apt-get update
+	echo -e "\033[42;37m --- Done to update --- \033[0m"
+fi
 
+echo -e "\033[43;37m --- apt-get runlib --- \033[0m"
 sudo apt-get install -y  \
 	rsync \
 	wget \
- 	zip \
+	zip \
 	unzip \
 	zstd \
 	git \
@@ -21,12 +26,16 @@ sudo apt-get install -y  \
 	libfreetype-dev \
 	libbrotli-dev \
 
+echo -e "\033[42;37m --- Done to runlib --- \033[0m"
 
+echo -e "\033[43;37m --- Dowload depend --- \033[0m"
 mkdir -p dependencies
 cd dependencies
 
 # Getting SDL
 if [ ! -d SDL* ]; then
+	echo ""
+	echo -e "\033[32m SDL \033[0m"
 	pushd .
 	wget https://www.libsdl.org/release/SDL-1.2.15.tar.gz -O- | tar xfz -
 	cd SDL*
@@ -41,10 +50,13 @@ if [ ! -d SDL* ]; then
 	make
 	sudo make install
 	popd
+	echo -e "\033[32m Done \033[0m"
 fi
 
 # Getting zlib
 if [ ! -d zlib* ]; then
+	echo ""
+	echo -e "\033[32m ZLIB \033[0m"
 	pushd .
 	wget http://zlib.net/fossils/zlib-1.2.12.tar.gz -O- | tar xfz -
 	cd zlib*
@@ -58,10 +70,13 @@ if [ ! -d zlib* ]; then
 		PREFIX=$host- \
 		install
 	popd
+	echo -e "\033[32m Done \033[0m"
 fi
 
 # Getting libpng
 if [ ! -d l*png* ]; then
+	echo ""
+	echo -e "\033[32m LIBPNG \033[0m"
 	pushd .
 	wget http://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz -O- | tar xfz -
 	cd l*png*
@@ -73,17 +88,23 @@ if [ ! -d l*png* ]; then
 	make
 	sudo make install
 	popd
+	echo -e "\033[32m Done \033[0m"
 fi
 
 # Getting discord_game_sdk
 if [ ! -d discord_game_sdk ]; then
+	echo ""
+	echo -e "\033[32m DISCORD_GAME_SDK \033[0m"
 	wget https://github.com/X-Lives/EasierOLCompile/releases/download/Releases/discord_game_sdk.zip
 	unzip -d discord_game_sdk discord_game_sdk.zip
 	rm discord_game_sdk.zip
+	echo -e "\033[32m Done \033[0m"
 fi
 
 # freetype lib for linux-windows cross compile
 if [ ! -d freetype* ]; then
+	echo ""
+	echo -e "\033[32m FREETYPE \033[0m"
 	pushd .
 	wget https://sourceforge.net/projects/freetype/files/freetype2/2.13.2/freetype-2.13.2.tar.gz/download -O- | tar xfz -
 	cd freetype*
@@ -97,4 +118,7 @@ if [ ! -d freetype* ]; then
 	make
 	sudo make install
 	popd
+	echo -e "\033[32m Done \033[0m"
 fi
+
+echo -e "\033[42;37m --- Done to depend --- \033[0m"
---
+++ install.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+
+EOLC_version=$(cat VERSION)
+if [[ $EOLC_time == "" ]];then
+    EOLC_time=$(date "+%Y-%m-%d %H:%M:%S")
+fi
+EOLC_install=1
+
+# echo "
+# _//      _//      _//                                     
+#  _//   _//        _//       _/                            
+#   _// _//         _//         _//     _//   _//     _//// 
+#     _//     _/////_//      _// _//   _//  _/   _// _//    
+#   _// _//         _//      _//  _// _//  _///// _//  _/// 
+#  _//   _//        _//      _//   _/_//   _/            _//
+# _//      _//      _////////_//    _//      _////   _// _//
+# "
+echo "
+  ______             _                ____   _         _____                          _  _       
+ |  ____|           (_)              / __ \ | |       / ____|                        (_)| |      
+ | |__    __ _  ___  _   ___  _ __  | |  | || |      | |      ___   _ __ ___   _ __   _ | |  ___ 
+ |  __|  / _\` |/ __|| | / _ \| '__| | |  | || |      | |     / _ \ | '_ \` _ \ | '_ \ | || | / _ \\
+ | |____| (_| |\__ \| ||  __/| |    | |__| || |____  | |____| (_) || | | | | || |_) || || ||  __/
+ |______|\__,_||___/|_| \___||_|     \____/ |______|  \_____|\___/ |_| |_| |_|| .__/ |_||_| \___|
+                                                                              | |                
+                                                                              |_|                
+"
+echo -e "\033[32mEasierOLCompile\033[0m version \033[33m${EOLC_version}\033[0m(create ${EOLC_time})"
+echo ""
+
+read -p "Next?[Y/n]" EOLC_re
+EOLC_re=$(echo $EOLC_re | tr '[:upper:]' '[:lower:]')
+if [[ $EOLC_re = *n* ]];then
+    exit 1
+fi
+echo ""
+
+while [[ "${ELOC_dir_ok}" != 1 ]];do
+    EOLC_dir=$(pwd)
+    read -p "EOLC_Directory[${EOLC_dir}|ENTER:default]: " EOLC_dir_read
+    if [ -n "${EOLC_dir_read}" ];then
+        # if not empty.
+        EOLC_dir=${EOLC_dir_read}
+    fi
+    if [ ! -d "${EOLC_dir}" ];then
+        echo -e "\033[31mError: This is not the valid directory!\033[0m"
+    elif [ "$(ls -A "$EOLC_dir" | grep -v ^install\.sh$)" ];then
+        echo -e "\033[31mError: This is not the empty directory!\033[0m"
+    else
+        ELOC_dir_ok=1
+    fi
+done
+while [[ "${EOLC_ds_ok}" != 1 ]];do
+    EOLC_ds=1
+    read -p "EOLC_Download_Source[1:Github|2:Gitee|ENTER:Github]: " EOLC_ds_read
+    if [[ "${EOLC_ds_read}" = 1 ]] || [[ "${EOLC_ds_read}" = 12 ]];then
+        EOLC_ds=${EOLC_ds_read}
+        EOLC_ds_ok=1
+    else
+        echo -e "\033[31mError: ${EOLC_ds_read} is not the valid number!\033[0m"
+    fi
+done
+if [[ "${EOLC_ds_read}" = 1 ]];then
+    EOLC_ds_link="https://github.com/X-Lives/EasierOLCompile"
+    SPEEDEDUP_LINK=""
+    CODE_REPO="https://github.com/X-Lives/X-Lives.git"
+    CODE_BRANCH=""
+    DATA_REPO="https://github.com/X-Lives/XLivesData.git"
+    DATA_BRANCH=""
+    GEMS_REPO="https://github.com/X-Lives/X-minorGems.git"
+    GEMS_BRANCH=""
+if [[ "${EOLC_ds_read}" = 2 ]];then
+    EOLC_ds_link=""
+fi
+
+echo ""
+echo "---------------------------"
+echo "EOLC_Directory: ${EOLC_dir}"
+echo "EOLC_Download_Source: ${EOLC_ds}"
+echo "---------------------------"
+read -p "OK?[ENTER:next]"
+echo "Will start..."
+read -t 1 -p "5s"
+read -t 1 -p "4s"
+read -t 1 -p "3s"
+read -t 1 -p "2s"
+read -t 1 -p "1s"
+echo ""
+echo ""
+echo ""
+
+sudo apt-get update
+sudo apt-get install -y git
+cd "${EOLC_dir}"
+git clone "${EOLC_ds_link}" miniOneLifeCompile
+cd miniOneLifeCompile
+echo "&{EOLC_time}" > CREATETIME
+chmod +x getDependencies.sh
+chmod +x cloneRepos.sh
+./getDependencies.sh
+./cloneRepos.sh
